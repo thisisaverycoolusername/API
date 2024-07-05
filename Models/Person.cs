293e497c592c24f177aec3d1a85dbd8931373973
Martin Lindenhöök <141586150+thisisaverycoolusername@users.noreply.github.com>
2024-07-05 08:58:25 +0200

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+       
+        public string? PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public IEnumerable<PersonWithInterests>? Interests { get; set; }
+    }
+}
---
+++ Models/PersonWithInterests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class PersonWithInterests
+    {
+        //Intresse
+        
+        public int Id { get; set; }
+        public IEnumerable<Link> Links { get; set; }
+        [ForeignKey("Person")]
+        public int PersonId { get; set; }
+        [ForeignKey("Interest")]
+        public int InterestId { get; set; }
+        public Interest? Interest { get; set; }
+    }
+}
